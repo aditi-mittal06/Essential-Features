363167944acebbb471ac616b4bf4c76316404fc4
Aditi <amittal@covalience.com>
2025-07-02 18:57:11 +0530

    Added @angular_material_dialog.js

---
+++ .angular/cache/19.2.15/app_without_ngrx/vite/deps/@angular_material_tooltip.js
@@ -0,0 +1,102 @@
+import {
+  MAT_TOOLTIP_DEFAULT_OPTIONS,
+  MAT_TOOLTIP_DEFAULT_OPTIONS_FACTORY,
+  MAT_TOOLTIP_SCROLL_STRATEGY,
+  MAT_TOOLTIP_SCROLL_STRATEGY_FACTORY,
+  MAT_TOOLTIP_SCROLL_STRATEGY_FACTORY_PROVIDER,
+  MatTooltip,
+  MatTooltipModule,
+  SCROLL_THROTTLE_MS,
+  TOOLTIP_PANEL_CLASS,
+  TooltipComponent,
+  getMatTooltipInvalidPositionError
+} from "./chunk-OCDAGUXG.js";
+import "./chunk-OIBNGD5S.js";
+import "./chunk-C2XA4KRZ.js";
+import "./chunk-MZOXJOR7.js";
+import "./chunk-42FJBLFI.js";
+import "./chunk-IBYU652R.js";
+import "./chunk-3JUAEHOX.js";
+import "./chunk-K7O4CQGI.js";
+import "./chunk-2O4WY5GE.js";
+import "./chunk-D2NIUPCM.js";
+import "./chunk-VP2Z6FFH.js";
+import "./chunk-GHJU2K5H.js";
+import "./chunk-QM37BOWQ.js";
+import "./chunk-RE4EY36O.js";
+import "./chunk-DKUKIZHH.js";
+import "./chunk-NZNOF4L5.js";
+import "./chunk-ZYKX7RMX.js";
+import "./chunk-DZ7BV6I4.js";
+
+// node_modules/@angular/material/fesm2022/tooltip.mjs
+var matTooltipAnimations = {
+  // Represents:
+  // trigger('state', [
+  //   state('initial, void, hidden', style({opacity: 0, transform: 'scale(0.8)'})),
+  //   state('visible', style({transform: 'scale(1)'})),
+  //   transition('* => visible', animate('150ms cubic-bezier(0, 0, 0.2, 1)')),
+  //   transition('* => hidden', animate('75ms cubic-bezier(0.4, 0, 1, 1)')),
+  // ])
+  /** Animation that transitions a tooltip in and out. */
+  tooltipState: {
+    type: 7,
+    name: "state",
+    definitions: [{
+      type: 0,
+      name: "initial, void, hidden",
+      styles: {
+        type: 6,
+        styles: {
+          opacity: 0,
+          transform: "scale(0.8)"
+        },
+        offset: null
+      }
+    }, {
+      type: 0,
+      name: "visible",
+      styles: {
+        type: 6,
+        styles: {
+          transform: "scale(1)"
+        },
+        offset: null
+      }
+    }, {
+      type: 1,
+      expr: "* => visible",
+      animation: {
+        type: 4,
+        styles: null,
+        timings: "150ms cubic-bezier(0, 0, 0.2, 1)"
+      },
+      options: null
+    }, {
+      type: 1,
+      expr: "* => hidden",
+      animation: {
+        type: 4,
+        styles: null,
+        timings: "75ms cubic-bezier(0.4, 0, 1, 1)"
+      },
+      options: null
+    }],
+    options: {}
+  }
+};
+export {
+  MAT_TOOLTIP_DEFAULT_OPTIONS,
+  MAT_TOOLTIP_DEFAULT_OPTIONS_FACTORY,
+  MAT_TOOLTIP_SCROLL_STRATEGY,
+  MAT_TOOLTIP_SCROLL_STRATEGY_FACTORY,
+  MAT_TOOLTIP_SCROLL_STRATEGY_FACTORY_PROVIDER,
+  MatTooltip,
+  MatTooltipModule,
+  SCROLL_THROTTLE_MS,
+  TOOLTIP_PANEL_CLASS,
+  TooltipComponent,
+  getMatTooltipInvalidPositionError,
+  matTooltipAnimations
+};
+//# sourceMappingURL=@angular_material_tooltip.js.map
