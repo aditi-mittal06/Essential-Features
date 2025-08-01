6c9edf90cd778bc984300398dbeeb18c29813112
Aditi <amittal@covalience.com>
2025-08-01 10:33:06 +0530

    Added chunk-GB5R5DJG.js.map

---
+++ .angular/cache/19.2.15/app_without_ngrx/vite/deps/graphql-tag.js
@@ -0,0 +1,18 @@
+import {
+  disableExperimentalFragmentVariables,
+  disableFragmentWarnings,
+  enableExperimentalFragmentVariables,
+  gql,
+  lib_default,
+  resetCaches
+} from "./chunk-V7JND6NU.js";
+import "./chunk-ZYKX7RMX.js";
+import "./chunk-DZ7BV6I4.js";
+export {
+  lib_default as default,
+  disableExperimentalFragmentVariables,
+  disableFragmentWarnings,
+  enableExperimentalFragmentVariables,
+  gql,
+  resetCaches
+};
